039271401b9be2e25b4935f2282eec9bace02706
mr_destructive <gormeet711@gmail.com>
2023-03-06 21:32:23 +0530

    test api

--- frontend/src/components/API.svelte
+++ frontend/src/components/API.svelte
@@ -1,14 +1,31 @@
 <script>
-    import h from 'hyperscript';
-
     const apiUrl = 'http://localhost:8000/';
     const query = '{authors{id,username}}';
     const endpoint = `${apiUrl}?query=${encodeURIComponent(query)}`
+  import { onMount, afterUpdate } from 'svelte';
+
+  let element;
+
+  function handleElementChange() {
+    console.log('Element has changed!');
+  }
+
+  onMount(() => {
+    element.addEventListener('change', handleElementChange);
+  });
+
+    afterUpdate(() => {
+        if (element.value) {
+          previousValue = element.value;
+          console.log('Element value has changed!');
+        }
+      });
 </script>
+
 <button _="on click fetch `{endpoint}` then
                     put the result as an JSON 
                     into next <p/> ">
   Get authors
 </button>
 
-<p id="authors"></p>
+    <p id="authors" bind:this={element} ></p>
